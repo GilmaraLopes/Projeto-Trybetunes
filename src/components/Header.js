501b5eb9bad124cbeeb33d74da3b232b3fa1106b
Gilmara <gilmaralopes2006@gmail.com>
2022-10-04 15:48:35 -0300

    requisito 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
 
 export default class Header extends Component {
+  state = {
+    loading: true,
+    nome: '',
+  };
+
+  componentDidMount() {
+    getUser().then((name) => {
+      this.setState({
+        loading: false,
+        nome: name.name,
+      });
+    });
+  }
+
   render() {
+    const { nome, loading } = this.state;
     return (
       <header data-testid="header-component">
-        <p data-testid="header-user-name"> </p>
+        <div data-testid="header-user-name">
+          {loading ? <Loading /> : nome}
+        </div>
         <BrowserRouter>
-          <Link data-testid="link-to-search" to="/search">search</Link>
-          <Link data-testid="link-to-favorites" to="/favorites">favoritas</Link>
-          <Link data-testid="link-to-profile" to="/profile">profile</Link>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favoritas</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
         </BrowserRouter>
       </header>
     );
