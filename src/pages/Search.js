8b6c2074aa2c9576118afcb8b6a99dcc5572ec97
Gilmara <gilmaralopes2006@gmail.com>
2022-10-04 15:19:40 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    // nome: '',
+    isButtonDisable: true,
+
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+      isButtonDisable: (target.value.length < 2),
+    });
+  };
+
   render() {
+    const { isButtonDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="pesquisa">
+            Nome do artista ou banda:
+            <input
+              type="text "
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              // onClick={}
+              data-testid="search-artist-button"
+              disabled={ isButtonDisable }
+            >
+              Procurar
+
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
