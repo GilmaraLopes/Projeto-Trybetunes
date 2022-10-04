f3c5bb040aa22b937efcd24ee7f7f236bcc96e12
Gilmara <gilmaralopes2006@gmail.com>
2022-10-04 00:00:56 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
         <Route path="/favorites" component={ Favorites } />
         <Route exact path="/profile" component={ Profile } />
         <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route exact path="" component={ NotFound } />
+        <Route path="*" component={ NotFound } />
       </BrowserRouter>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
 
+// import userAPI from './services/userAPI';
+
 export default class Login extends Component {
+  state = {
+    nome: '',
+    isButtonDisable: true,
+  };
+
+  enableBtn = () => {
+    const { nome } = this.state;
+    const usuario = nome.length > 2;
+    this.setState({ isButtonDisable: !usuario });
+  };
+
+  handleChange = ({ target }) => {
+    console.log(target);
+    this.setState({
+      [target.name]: target.value,
+    }, () => this.enableBtn());
+  };
+
   render() {
+    const { nome, isButtonDisable } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+
+      <form>
+        <div data-testid="page-login" />
+        <label htmlFor="login-usuario">
+          Nome:
+          <input
+            type="text "
+            name="nome"
+            value={ nome }
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            onClick={ this.createUser }
+            data-testid="login-submit-button"
+            disabled={ isButtonDisable }
+          >
+            Entrar
+
+          </button>
+        </label>
+      </form>
     );
   }
 }
