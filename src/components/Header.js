e7581946406598e36d293c41aeb03cd53c1de8af
Gilmara <gilmaralopes2006@gmail.com>
2022-10-04 11:51:41 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     return (
       <header data-testid="header-component">
         <p data-testid="header-user-name"> </p>
+        <BrowserRouter>
+          <Link data-testid="link-to-search" to="/search">search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">favorites</Link>
+          <Link data-testid="link-to-profile" to="/profile">profile</Link>
+        </BrowserRouter>
       </header>
     );
   }
