c5d237b3c35695f04bb8e743bc989cec3a39be5a
Gilmara <gilmaralopes2006@gmail.com>
2022-10-05 11:10:02 -0300

    requisito 7

---
+++ src/components/MusicCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class MusicCard extends Component {
+  render() {
+    return (
+      <div>
+        <audio data-testid="audio-component" src="{previewUrl}" controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
+  state = {
+    id: '',
+    teste: '',
+    albumList: [],
+    // mostraInfo: false,
+
+  };
+
+  // handleChange = ({ target }) => {
+  //   const { name, value } = target;
+  //   this.setState({
+  //     [name]: value,
+  //     isButtonDisable: (value.length < 2),
+  //   });
+  // };
+
+  requestMusics = async () => {
+    const { id } = this.state;
+    const response = await getMusics(id);
+    console.log(response);
+    this.setState({
+      teste: id,
+      albumList: response,
+      id: '',
+    });
+  };
+
   render() {
     return (
       <div data-testid="page-album">
-
+        {/* { albumList.map((element) => (
+            <div key={ element.collectionId }>
+            <p data-testid="artist-name">Nome da banda/Artista</p>
+            <p data-testid="album-name">Nome do Album/Artista</p>
+       } */}
         <Header />
       </div>
     );
